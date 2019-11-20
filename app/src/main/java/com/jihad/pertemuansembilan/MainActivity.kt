b28e6b5a610c49ec079f5df7f89d3e0da68e9669
Saodah1202 <oodah.12@gmail.com>
2019-11-20 23:59:59 +0700

    first commit

---
+++ app/src/main/java/com/jihad/pertemuansembilan/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.jihad.pertemuansembilan
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+
+    //Fungsi yang pertama kali di jalankan
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        view_pager.adapter = ViewPagerAdapter(
+            this, supportFragmentManager)
+        tabs.setupWithViewPager(view_pager)
+    }
+}
